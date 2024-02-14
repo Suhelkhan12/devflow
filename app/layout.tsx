07485ee399d17e956e63608b7fca434b51d4a272
Suhel <suhel7822@gmail.com>
2024-02-14 23:24:11 +0530

    implement context for dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import ThemeProvider from "@/context/ThemeProvider";
 
 // this is how next js uses font from google fonts
 const inter = Inter({
@@ -32,19 +33,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: "primary-gradient",
-          footerActionLink: "primary-text-gradient hover:text-priary-500",
-        },
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-priary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useContext, createContext, useState, useEffect } from "react";
+
+interface ThemeContextType {
+  mode: string | null;
+  setMode: (mode: "dark" | "light" | null) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  // setting mode for theme
+  const [mode, setMode] = useState<"dark" | "light" | null>(null);
+
+  useEffect(() => {
+    // function for setting the theme
+    const handleTheme = () => {
+      if (mode === "dark") {
+        setMode("light");
+        document.documentElement.classList.add("light");
+      } else {
+        setMode("dark");
+        document.documentElement.classList.add("dark");
+      }
+    };
+
+    handleTheme();
+  }, [mode]);
+
+  return (
+    // basically here we have given mode and setMode to the value of provider
+    // so that they will be available to us in any component wrapped inside the provider
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("usetheme error");
+  }
+
+  return context;
+}
