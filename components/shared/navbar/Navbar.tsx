869e8870205884294fbcdd0acff80dfd53905631
Suhel <suhel7822@gmail.com>
2024-02-19 21:55:56 +0530

    implement basic nav structure

---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,50 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/assets/images/site-logo.svg"
+          alt="Devflow"
+          width={24}
+          height={24}
+        />
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev <span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
+      GlobalSearch
+      <div className="flex-between gap-5">
+        Theme
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+        {/* 
+        here mobile navigation will come */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+/**
+ * DOCUMENTATION
+ * 1) Clerk provide us with a SignedIn component which will only show user profile option only if the user is signed in if user is not signed in then it will not show the user profile button
+ *
+ *
+ */
