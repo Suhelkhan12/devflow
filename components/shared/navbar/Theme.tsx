4ab3e07df4d6e6c9c0e162c5e7e0fc71fe0f5428
Suhel <suhel7822@gmail.com>
2024-03-07 00:20:28 +0530

    make data for tags and implement tag in the UI

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -37,7 +37,7 @@ const Theme = () => {
             />
           )}
         </MenubarTrigger>
-        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
+        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border bg-white py-2 dark:border-dark-400 dark:bg-dark-300">
           {themes.map((theme) => (
             <MenubarItem
               key={theme.value}
--- components/shared/sidebars/RightSidebar.tsx
+++ components/shared/sidebars/RightSidebar.tsx
@@ -26,6 +26,44 @@ const RightSidebar = () => {
       title: "What is an example of 3 numbers that do not make up a vector?",
     },
   ];
+
+  const popularTags = [
+    {
+      _id: 1,
+      name: "Javascript",
+      totalQuestions: 34,
+    },
+    {
+      _id: 2,
+      name: "Typescript",
+      totalQuestions: 50,
+    },
+    {
+      _id: 3,
+      name: "Reactjs",
+      totalQuestions: 44,
+    },
+    {
+      _id: 4,
+      name: "Nextjs",
+      totalQuestions: 64,
+    },
+    {
+      _id: 5,
+      name: "Nodejs",
+      totalQuestions: 100,
+    },
+    {
+      _id: 6,
+      name: "Tailwind",
+      totalQuestions: 11,
+    },
+    {
+      _id: 7,
+      name: "Mongodb",
+      totalQuestions: 45,
+    },
+  ];
   return (
     <section className=" background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[330px] flex-col justify-between overflow-y-auto border-l p-6 pt-32 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div className="flex h-full flex-col justify-between gap-16">
@@ -57,15 +95,9 @@ const RightSidebar = () => {
         <div>
           <h3 className="h3-bold text-dark200_light900">Top questions</h3>
           <div className="mt-7 flex w-full flex-col gap-4">
-            <Tag />
-            <Tag />
-            <Tag />
-            <Tag />
-            <Tag />
-            <Tag />
-            <Tag />
-            <Tag />
-            <Tag />
+            {popularTags.map((tag) => (
+              <Tag key={tag._id} {...tag} />
+            ))}
           </div>
         </div>
       </div>
