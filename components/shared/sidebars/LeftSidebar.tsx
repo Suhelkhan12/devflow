53c36f0e72ca12053313db93f4ada2c2bf7eb482
Suhel <suhel7822@gmail.com>
2024-03-06 23:24:13 +0530

    change layout problem of leftsidebar

--- components/shared/sidebars/LeftSidebar.tsx
+++ components/shared/sidebars/LeftSidebar.tsx
@@ -10,35 +10,37 @@ const LeftSidebar = () => {
   const path = usePathname();
   return (
     <section className=" background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-32 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
-      <div className="flex h-full flex-col gap-4 pt-8">
-        {sidebarLinks.map((link) => {
-          const isActive =
-            path === link.route ||
-            (path.includes(link.route) && link.route.length > 1);
-          return (
-            <div key={link.route}>
-              <Link
-                href={link.route}
-                className={`${
-                  isActive
-                    ? "primary-gradient rounded-lg text-light-900"
-                    : "text-dark300_light900"
-                } flex items-center justify-start gap-4 bg-transparent p-2`}
-              >
-                <Image
-                  src={link.imgURL}
-                  alt={link.label}
-                  width={20}
-                  height={20}
-                  className={`${isActive ? "" : "invert-colors"}`}
-                />
-                <p className={`${isActive ? "base-bold" : "base-medium"}`}>
-                  {link.label}
-                </p>
-              </Link>
-            </div>
-          );
-        })}
+      <div className="flex h-full flex-col justify-between">
+        <div className="flex flex-col gap-4">
+          {sidebarLinks.map((link) => {
+            const isActive =
+              path === link.route ||
+              (path.includes(link.route) && link.route.length > 1);
+            return (
+              <div key={link.route}>
+                <Link
+                  href={link.route}
+                  className={`${
+                    isActive
+                      ? "primary-gradient rounded-lg text-light-900"
+                      : "text-dark300_light900"
+                  } flex items-center justify-start gap-4 bg-transparent p-2`}
+                >
+                  <Image
+                    src={link.imgURL}
+                    alt={link.label}
+                    width={20}
+                    height={20}
+                    className={`${isActive ? "" : "invert-colors"}`}
+                  />
+                  <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                    {link.label}
+                  </p>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
         <SignedOut>
           <div className="flex flex-col gap-3">
             <div>
