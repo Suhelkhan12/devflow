c59576fa697e32ad72ec73e61f2536e16390bedd
Suhel <suhel7822@gmail.com>
2024-03-06 23:34:21 +0530

    make styles for right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "@/components/shared/navbar/Navbar";
 import LeftSidebar from "@/components/shared/sidebars/LeftSidebar";
+import RightSidebar from "@/components/shared/sidebars/RightSidebar";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="background-light850_dark100 relative">
@@ -10,7 +11,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        {/* rightSidebar */}
+        <RightSidebar />
       </div>
       {/* Toaster */}
     </main>
