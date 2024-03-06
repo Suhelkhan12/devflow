7d4d1e7da8e7e7de29426723899f1dbe9eea2ac6
Suhel <suhel7822@gmail.com>
2024-03-06 23:08:42 +0530

    add styles for left sidebars

--- components/shared/sidebars/LeftSidebar.tsx
+++ components/shared/sidebars/LeftSidebar.tsx
@@ -1,5 +1,7 @@
 const LeftSidebar = () => {
-  return <div className="lg:w-[266px]"></div>;
+  return (
+    <section className=" background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]"></section>
+  );
 };
 
 export default LeftSidebar;
