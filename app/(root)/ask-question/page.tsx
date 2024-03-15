7dd681a798d366ce2cf73d6cc7f8629a1e9df94f
Suhel <suhel7822@gmail.com>
2024-03-15 23:15:13 +0530

    implement basic form with zod and shadcn

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import QuestionForm from "@/components/forms/QuestionForm";
 
 const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+  return (
+    <div>
+      <h1 className=" h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
---
+++ components/forms/QuestionForm.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(3, {
+      message: "Name should have atleast 3 characters.",
+    })
+    .max(50, {
+      message: "Name too big.",
+    }),
+});
+
+const QuestionForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default QuestionForm;
