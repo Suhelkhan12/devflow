283d957e2adfd34022955a5e07b2e4088f43b2aa
Suhel <suhel7822@gmail.com>
2024-08-27 00:15:39 +0530

    user model change

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,35 +1,35 @@
 import { Schema, models, model, Document } from "mongoose";
 
 export interface IUser extends Document {
-    clerkId: string;
-    name: string;
-    username: string;
-    email: string;
-    password?: string;
-    bio?: string;
-    picture: string;
-    location: string;
-    portfolioWebsite?: string;
-    reputation?: number;
-    saved: Schema.Types.ObjectId[];
-    joinedAt: Date;
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  bio?: string;
+  picture: string;
+  location?: string;
+  portfolioWebsite?: string;
+  reputation?: number;
+  saved: Schema.Types.ObjectId[];
+  joinedAt: Date;
 }
 
 const UserSchema = new Schema<IUser>({
-    clerkId: { type: String, required: true },
-    name: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: false }, 
-    bio: { type: String, required: false }, 
-    picture: { type: String, required: true },
-    location: { type: String, required: true },
-    portfolioWebsite: { type: String, required: false }, 
-    reputation: { type: Number, default: 0 }, 
-    saved: [{ type: Schema.Types.ObjectId, ref: 'Question' }], 
-    joinedAt: { type: Date, default: Date.now },
+  clerkId: { type: String, required: true },
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: false },
+  bio: { type: String, required: false },
+  picture: { type: String, required: true },
+  location: { type: String, required: true },
+  portfolioWebsite: { type: String, required: false },
+  reputation: { type: Number, default: 0 },
+  saved: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  joinedAt: { type: Date, default: Date.now },
 });
 
-const User = models.User || model<IUser>('User', UserSchema);
+const User = models.User || model<IUser>("User", UserSchema);
 
 export default User;
